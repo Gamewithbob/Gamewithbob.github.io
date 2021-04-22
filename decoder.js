bf347f2b20ded10f74e9be512c86231db9fae708
Gamewithbob <bobbybaggett73@gmail.com>
2021-04-22 13:32:20 -0400

    some changes

--- decoder.js
+++ decoder.js
@@ -1,1 +1,5 @@
-var encoded = window.prompt()
+function decode () { var message = window.prompt("Type the encoded message");
+var decoded = (decode.replace  ("q", "a") ("w", "b") ("e", "c"));
+
+}
+console.clear;
