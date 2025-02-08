11b923aa256c236bd432fc80cbd58c01a035488b
Gamewithbob <bobbybaggett73@gmail.com>
2025-02-08 14:54:47 -0500

    ed

--- decoder.js
+++ decoder.js
@@ -3,8 +3,7 @@ var cipher=String("qwertyuiopasdfghjklzxcvbnm").split("");
 var alpha=String("abcdefghijklmnopqrstuvwxyz").split("");
 var res=[message.length]
 function replace(){
-//cipher set
-
+  
 for (let i=0;i>message.length;i++){
 for (let ii = 0; ii < alpha.length; ii++) {
   if (message[i]==alpha[ii]) {
